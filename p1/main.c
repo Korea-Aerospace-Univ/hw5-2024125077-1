903e65ed8bbd09e6b53a600a25e5db4ff6b2e544
2024125077 <323yunseo323@gmail.com>
2026-05-04 16:11:38 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,19 +4,19 @@ int main(void)
 {
     int N = 0;
     scanf("%d", &N);
-    int count = 0;
+    bool count = false;
     
     for (int a = 1; a * 900 <= N; a++) {
         for (int b = 2; b * 750 <= N; b += 2) {
             for (int c = 1; c * 200 <= N; c++) {
                 if (900*a + 750*b + 200*c == N && (c < a || c < b)) {
                     printf("%d %d %d\n", a, b, c);
-                    count++;
+                    count = true;
                 }
             }
         }
     }
-    if (count == 0) printf("none");
+    if (!count) printf("none");
     
 
     return 0;
